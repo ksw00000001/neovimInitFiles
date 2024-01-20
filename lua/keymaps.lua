2cf3579c6ed4d8f4c069678fc2a7f3a32424a51d
ksw001 <ricecrisper14@gmail.com>
2024-01-20 21:39:26 +0900

    neovim setting

---
+++ lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('i','{','{}<LEFT>')
+vim.keymap.set('i','(','()<LEFT>')
+vim.keymap.set('t','<ESC>','<C-\\><C-n><Plug>(esc)')
+vim.keymap.set('n','<Plug>(esc)<ESC>','i<ESC>')
---
+++ lua/settings.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true
+vim.opt.autoindent = true 
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+
